5551db65331d8ecb69e61781646825534282325a
PrathamRathod14 <p.rathod@ostfalia.de>
2026-01-26 12:58:58 +0100

    fix: import global tailwind css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
-import "sonner/dist/index.css";
+import "./index.css"; // ← THIS LINE IS REQUIRED
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
